3ffe49f5e94b8325e6c8bf39bf0d0c231bba1648
AJITH19233 <135130218+AJITH19233@users.noreply.github.com>
2024-01-02 01:29:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -96,11 +96,10 @@ app.delete("api/delete/:id",async(req,res)=>{
     res.send({success : true, message : "data delete successfully", data : data})
 })
 
-const path = require('path');
-app.use(express.static(path.join(_dirname,'/build')));
-app.get('/*',function(req,res){
-    res.sendFile(path.join(_dirname,'/build/index.html'));
-});
+const path = require('path'); 
+app.use(express.static(path.join(__dirname,'/build'))); 
+app.get('/*',function(req,res){ res.sendFile(path.join(__dirname,'/build/index.html')); });
+
 
 
 
